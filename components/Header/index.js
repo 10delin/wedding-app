c40c58d6624cf984b7fa25047ef0575dd039d210
Antonio Delage <delincruzado20@gmail.com>
2024-11-20 17:28:31 +0100

    refactor and header

---
+++ components/Header/index.js
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+
+const HeaderStyles = styled.div`
+  display: block;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: white;
+  z-index: 1;
+
+  @media (min-width: 768px) {
+    position: fixed;
+    bottom: initial;
+    top: 0;
+  }
+`;
+
+const HeaderContentStyles = styled.div`
+  display: flex;
+  align-items: center;
+  box-shadow: 0 -2px 4px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+  padding: 0 5%;
+
+  @media (min-width: 768px) {
+    padding: 0 10%;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  }
+
+  @media (min-width: 1024px) {
+    padding: 0 10%;
+  }
+
+  @media (min-width: 1440px) {
+    padding: 0 15%;
+  }
+`;
+
+const HeaderDescriptionStyles = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 1.2rem;
+  font-weight: 350;
+  width: 100%;
+  justify-content: space-evenly;
+  gap: 10px;
+
+  @media (min-width: 768px) {
+    justify-content: flex-end;
+    gap: 0;
+  }
+`;
+
+const HeaderItemStyles = styled.p`
+  margin-right: 10px;
+  position: relative;
+`;
+
+const HeaderItemLinkStyles = styled.a`
+  padding: 19px 3px;
+  text-decoration: none;
+  color: #0f104d;
+  font-size: 1.2rem;
+  font-weight: 600;
+  transition: color 0.3s;
+
+  @media (min-width: 768px) {
+    padding: 19px 15px;
+  }
+
+  &::after {
+    background: none repeat scroll 0 0 transparent;
+    top: -20px;
+    content: "";
+    display: block;
+    height: 3px;
+    left: 50%;
+    position: absolute;
+    background: #0f104d;
+    transition: width 0.3s ease 0s, left 0.3s ease 0s;
+    width: 0;
+
+    @media (min-width: 768px) {
+      bottom: -20px;
+      top: initial;
+      height: 2px;
+    }
+  }
+
+  &:hover {
+    background-color: #f5f5f5;
+    color: #2a85ee;
+  }
+
+  &:hover::after {
+    width: 100%;
+    left: 0;
+  }
+`;
+
+const Header = ({data}) => {
+  return (
+    <HeaderStyles>
+      <HeaderContentStyles>
+        <HeaderDescriptionStyles>
+          {data?.map((item) => (
+            <HeaderItemStyles key={item}>
+              <HeaderItemLinkStyles href={`#${item}`}>{item}</HeaderItemLinkStyles>
+            </HeaderItemStyles>
+          ))}
+        </HeaderDescriptionStyles>
+      </HeaderContentStyles>
+    </HeaderStyles>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
+import Header from "@/components/Header";
 import useFetchData from "../hooks/useFetchData";
 import Countdown from "@/components/Countdown";
 
 const Home = () => {
   const { data, loading, error } = useFetchData("/api/weddingInfo");
 
-  console.log(data);
-
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
     <>
-      <h1>{data?.title}</h1>
+      <Header data={data.header} />
+      <div style={{marginTop:"225px" }}>
+        HOLA
+        </div>
+      {/* <TitleStyles>{data?.title}</TitleStyles> */}
       <Countdown targetDate={data?.countdown?.date} />
       <div>
         <a href={data.link_form} target="_blank" rel="noreferrer">
